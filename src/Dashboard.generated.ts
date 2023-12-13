8b78f46f4a4d585dc520150e068a22711e29006c
gn-devops-ci <108643345+gn-devops-ci@users.noreply.github.com>
2023-12-13 00:19:35 +0000

    chore(deps): upgrade dependencies (#96)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/GoodNotes/ts-interface-generator/actions/runs/7188862961
    
    ------
    
    *Automatically created by projen via the "upgrade" workflow*

--- src/Dashboard.generated.ts
+++ src/Dashboard.generated.ts
@@ -99,7 +99,7 @@ export interface AlertGraphWidgetDefinition {
 
 export interface WidgetTime {
     /** The available timeframes depend on the widget you are using. */
-    readonly "liveSpan"?: "1m" | "5m" | "10m" | "15m" | "30m" | "1h" | "4h" | "1d" | "2d" | "1w" | "1mo" | "3mo" | "6mo" | "1y" | "alert" | UnparsedObject;
+    readonly "liveSpan"?: "1m" | "5m" | "10m" | "15m" | "30m" | "1h" | "4h" | "1d" | "2d" | "1w" | "1mo" | "3mo" | "6mo" | "week_to_date" | "month_to_date" | "1y" | "alert" | UnparsedObject;
 }
 
 export interface AlertValueWidgetDefinition {
